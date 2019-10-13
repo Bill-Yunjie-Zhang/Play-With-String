ec633a4c68d6c800e1eb6da1edf688feef2e4a87
Bill <207559339@qq.com>
2019-10-13 13:30:39 +0800

    reviewing in the day4

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// var string = "This is a great city"
+// console.log(string.length);
+// console.log(string.indexOf("a"));
+// console.log(string.lastIndexOf("a"))
+// console.log(string.slice(10,15))
+
+let number = 123434.98765;
+console.log(typeof number.toString())
+console.log(typeof number.toPrecision(5))
+
+let boolean = true;
+console.log(typeof boolean.toString())
